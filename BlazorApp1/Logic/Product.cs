02277d6c856e932af3ae5bf51805adb92549712e
oleeriksen <oleeriksen1958@gmail.com>
2026-09-24 11:51:32 +0200

    forms

---
+++ BlazorApp1/Logic/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace Logic;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Product
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Range(1,int.MaxValue, ErrorMessage = "Price must be positive")]
+        public int? Price { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+        
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+        
+    }
+
+
